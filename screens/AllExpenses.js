df0589df168faeae1dfa55ef2dd7ff3499551c4b
Kaushik Sharma <kaushik445566@gmail.com>
2024-06-27 00:39:23 +0530

    global state management

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { useContext } from "react";
 
-const AllExpenses = () => {
-  return <ExpensesOutput expensesPeriod="Total" />;
-};
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  );
+}
 
 export default AllExpenses;
-
-const styles = StyleSheet.create({});
--- util/date.js
+++ util/date.js
@@ -2,3 +2,6 @@ import { format } from "date-fns";
 export function getFormattedDate(date) {
   return format(date, "PPp");
 }
+export function getDateMinusDays(date, days) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+}
